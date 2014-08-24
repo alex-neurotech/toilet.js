f04bcde18a80a2c10ece9253d6ab888f7cdc835f
Daryn St. Pierre <daryn@bloqmail.com>
2014-08-23 23:18:09 -0400

    Fixed dumb stuff.

--- toilet.js
+++ toilet.js
@@ -1,31 +1,37 @@
-function toiletSpinner() {
-	/**
-	 * Required variables
-	 */
-	var toilet = $('body'),
-		flusher = $('<a id="flusherHandle">Flush me!</a>');
+$(function() {
 
-	flusher.css({
-		'color' : 'red',
-		'cursor' : 'pointer',
-		'font-weight' : 'bold',
-		'position' : 'absolute',
-		'top' : '20px',
-		'right' : '20px'
-	});
+	function toiletSpinner() {
+		/**
+		 * Required variables
+		 */
+		var toilet = $('body'),
+			flusher = $('<a id="flusherHandle">Flush me!</a>');
 
-	toilet.prepend(flusher);
-
-	flusher.on( 'click', function() {
-		toilet.css({
-			'font-size' : '0px',
-			'-webkit-transition:' : '2s all ease-in-out',
-			'-moz-transition' : '2s all ease-in-out',
-			'transition' : '2s all ease-in-out',
-			'-webkit-transform': 'rotate(360deg)',
-			'-moz-transform' : 'rotate(360deg)',
-			'-o-transform' : 'rotate(360deg)',
-			'-ms-transform' : 'rotate(360deg)'
+		flusher.css({
+			'color' : 'red',
+			'cursor' : 'pointer',
+			'font-weight' : 'bold',
+			'position' : 'absolute',
+			'top' : '20px',
+			'right' : '20px'
 		});
-	});
-}
+
+		toilet.prepend(flusher);
+
+		flusher.on( 'click', function() {
+			toilet.css({
+				'font-size' : '0px',
+				'-webkit-transition:' : '2s all ease-in-out',
+				'-moz-transition' : '2s all ease-in-out',
+				'transition' : '2s all ease-in-out',
+				'-webkit-transform': 'rotate(360deg)',
+				'-moz-transform' : 'rotate(360deg)',
+				'-o-transform' : 'rotate(360deg)',
+				'-ms-transform' : 'rotate(360deg)'
+			});
+		});
+	}
+
+	toiletSpinner();
+
+});
